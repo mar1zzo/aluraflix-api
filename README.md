1dc73154993e906b7936a5090acd7eea9a4df8d0
Mariana Rizzo <morallesrizzo@gmail.com>
2021-07-21 19:25:25 -0300

    Initial commit: API structure and POST request

---
+++ README.md
@@ -0,0 +1,2 @@
+# aluraflix-api
+API Rest com .NET Core 5 criada no “Alura Challenge BackEnd” com objetivo de atender uma plataforma de compartilhamento de vídeos.
---
+++ aluraflix-api/Controllers/VideoController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using aluraflix_api.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace aluraflix_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class VideoController : ControllerBase
+    {
+        private static List<Video> videos = new List<Video>();
+
+        [HttpPost]
+        public void AddVideo([FromBody] Video video)
+        {
+            videos.Add(video);
+        }
+    }
+}
